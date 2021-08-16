d245791b68109109948bf82b78f69c3d6277d8d9
Mainman002 <Mainman002@users.noreply.github.com>
2021-08-16 01:11:11 -0500

    Add min window sizie
    * Add electron game window minimum size (480x320)

--- core-defender/main.js
+++ core-defender/main.js
@@ -9,8 +9,6 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
-    minWidth: 900+14,
-    minHeight: 600+41,
     autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
@@ -23,7 +21,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
